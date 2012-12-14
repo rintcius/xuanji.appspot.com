5434a182a26c8a7f885bfa7bdbe7ef3e6bdf3a11
Li Xuanji <xuanji@gmail.com>
2012-12-14 23:56:20 +0800

    completely remove lispy

--- index.py
+++ index.py
@@ -10,7 +10,6 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-from lispy import lis, bf
 from python_grading.python import python
 
 from google.appengine.ext.webapp import template
@@ -20,35 +19,11 @@ class MainPage(webapp.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, {}))
 
-
-class lispy(webapp.RequestHandler):
-
-    def get(self):
-        self.response.out.write("""
-          <html>
-            <body>
-              <form action="/lispy" method="post">
-                <div><textarea name="content" rows="3" cols="60"></textarea></div>
-                <div><input type="submit" value="submit"></div>
-              </form>
-            </body>
-          </html>""")
-          
-    def post(self):
-        self.response.out.write('<html><body>Output:<pre>')
-        content = self.request.get('content')
-        out = str(lis.eval(lis.parse(content)))
-        
-        self.response.out.write(cgi.escape('> ' + str(content)) + '\n')
-        self.response.out.write(cgi.escape(out))
-        self.response.out.write('</pre></body></html>')
         
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
-                                      ('/lispy', lispy),
                                       ('/python(.*)', python)],
                                       debug=True)
-
 def main():
     run_wsgi_app(application)
 
